13c8ead6ed7643f2a31137700a7951958e14c095
mihirishanika <168528756+mihirishanika@users.noreply.github.com>
2025-06-15 20:33:16 +0530

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Calculate Simple Interest
+
+echo "Enter Principal:"
+read P
+echo "Enter Rate of Interest:"
+read R
+echo "Enter Time (in years):"
+read T
+
+SI=$(echo "scale=2; ($P * $R * $T) / 100" | bc)
+echo "Simple Interest is: $SI"
